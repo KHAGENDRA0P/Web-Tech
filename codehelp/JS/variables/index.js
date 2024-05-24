7864043916f9732565c83047e6b59d039dc3b18d
KHAGENDRA <mohanta.khagendranath@gmail.com>
2024-05-24 10:02:42 +0530

    Variables

---
+++ codehelp/JS/variables/index.js
@@ -0,0 +1,20 @@
+// var age = 20;
+
+// if(true){
+//     console.log(age);
+// }
+
+// function solve(){
+//     var age = 25;
+//     console.log(age);
+// }
+// console.log(age);
+
+// solve();
+// {
+// let a = 69;
+// }
+// console.log(a);
+
+const b = 29;
+console.log(b);
