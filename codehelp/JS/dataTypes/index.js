66680711b5332bc20c6f41831889049b34af6a4a
KHAGENDRA <mohanta.khagendranath@gmail.com>
2024-05-24 10:01:55 +0530

    DataTypes

---
+++ codehelp/JS/dataTypes/index.js
@@ -0,0 +1,9 @@
+let marks = null ;
+// marks="Khagendra"
+console.log(marks);
+console.log(typeof(marks));
+
+const value1 = Symbol('hi');
+const value2 = Symbol('hi');
+
+console.log(value1 === value2); // false
