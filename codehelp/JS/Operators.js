06b472bf35a4e90e862819e9b3fd5c55daf39db4
KHAGENDRA <mohanta.khagendranath@gmail.com>
2024-05-25 12:03:28 +0530

    Operators

--- codehelp/JS/Operators.js
+++ codehelp/JS/Operators.js
@@ -1,2 +1,34 @@
 //Arithmatic Operator
-let a=10;
+let a=2;
+let b=5;
+
+console.log(a**b);
+
+// a=a+10;
+a*=10;
+
+'10'==10
+'10'===10
+
+let age = 24;
+let status = (age>18)?'Can Vote':'Cant Vote'
+console.log(status);
+
+// let ans = (true && false && true)
+// let ans = (false || false || false)
+let ans = !(false )
+console.log(ans);
+
+console.log(false || 'Gopal');
+console.log(false || 7 || 23 || 9);
+
+console.log(2 & 5);
+console.log(2 | 5);
+console.log(2 ^ 2);
+console.log(~(0));
+console.log(5<<1);
+console.log(10>>1);
+
+
+
+
