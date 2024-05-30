a960b128a10ecbbfe2dd9baf24f4f407c804a31f
KHAGENDRA <mohanta.khagendranath@gmail.com>
2024-05-31 00:23:47 +0530

    Arrow Functions

--- codehelp/JS/function.js
+++ codehelp/JS/function.js
@@ -27,3 +27,45 @@ function getSum(a,b,c){
   let sum = a+b+c;
   return sum;
 }
+let ans = getSum(1,2,3);
+    console.log("print sum",ans)
+
+function getName(fName,lName){
+  let fullName = fName+' '+lName;
+  return fullName;
+  //unreachable statements
+  let sum = 10;
+}
+let fullname = getName("KAKASHI",'UCHIHA')
+    console.log(fullname)
+
+const multiplication = function(a,b){
+  return a*b;
+}
+let mult = multiplication(2,10);
+    console.log(mult);
+
+// function square(num){
+let square = function(num){
+  let sq = num**2;
+  return sq;
+}
+let sqr=square(5);
+    console.log(sqr)
+
+
+// function exp(x,y){
+//   let a = x**y;
+//   return a;
+// }
+//     console.log(exp(2,5))
+// let exp = function (x,y){
+//   let a = x**y;
+//   return a;
+// }
+//     console.log(exp(2,10));
+let exp = (x,y) => {
+  let a = x**y;
+  return a;
+}
+    console.log(exp(2,10));
