132d33162e8118a2c88325b3f0319a02b2e0d0e0
KHAGENDRA <mohanta.khagendranath@gmail.com>
2024-06-07 22:45:24 +0530

    Class & Object

---
+++ codehelp/JS/Classes.js
@@ -0,0 +1,32 @@
+class Human {
+  // properties
+  age = 13; //by default public
+  #wt = 80;
+  ht = 180;
+  
+  constructor(newAge,newHt,newWt){
+    this.age = newAge;
+    this.ht = newHt;
+    this.#wt = newWt;
+  }
+  // behaviour
+  #walk(){
+    console.log("I am walking" ,this.#wt)
+  }
+  run(){
+    console.log("I am runnong")
+  }
+  get fetchWt(){
+    return this.#wt;
+  }
+  
+  set modifyWt(val){
+    this.wt = val;
+  }
+}
+
+let obj = new Human(50,190,101);
+console.log(obj.age);
+console.log(obj.fetchWt);
+
+obj.walk();
