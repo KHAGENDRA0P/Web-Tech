b89eee0f3af6eca19017eeb5477d9145989e69e8
KHAGENDRA <mohanta.khagendranath@gmail.com>
2024-06-08 22:48:37 +0530

    math and date

---
+++ codehelp/JS/commonObject.js
@@ -0,0 +1,18 @@
+// Math Object
+console.log(Math.PI);
+console.log(Math.max(94,90,39,499,21));
+console.log(Math.min(94,90,39,499,21));
+console.log(Math.round(1.5));
+console.log(Math.floor(1.9));
+console.log(Math.ceil(1.01));
+console.log(Math.abs(-9));
+console.log(Math.random());
+console.log(Math.sqrt(49));
+console.log(Math.pow(2,6));
+
+// Date Object
+console.log(Date());
+let date = new Date();
+console.log(date.getMonth());
+console.log(date.setFullYear(2001));
+console.log(date);
