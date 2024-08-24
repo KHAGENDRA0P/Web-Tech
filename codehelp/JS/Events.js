de1ebef071898f0af2ad9d82d88bea9e3c1ba6ed
KHAGENDRA <mohanta.khagendranath@gmail.com>
2024-08-24 22:54:54 +0530

    event object

--- codehelp/JS/Events.js
+++ codehelp/JS/Events.js
@@ -1,4 +1,5 @@
-function changeText(){
+function changeText(event){
+    console.log(event)
     let fpara = document.getElementById('fpara');
     fpara.textContent="Hello Boys";
 }
@@ -6,3 +7,4 @@ let fpara = document.getElementById('fpara');
 fpara.addEventListener('click',changeText);
 // fpara.removeEventListener('click',changeText);
 // optimal way
+let anchor = document.getElementById("link");
