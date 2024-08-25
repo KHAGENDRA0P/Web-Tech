20e8786c3bbc6c5b5734ec0557035674f1fe7b68
KHAGENDRA <mohanta.khagendranath@gmail.com>
2024-08-25 12:25:54 +0530

    prevent default

--- codehelp/JS/Events.js
+++ codehelp/JS/Events.js
@@ -7,4 +7,28 @@ let fpara = document.getElementById('fpara');
 fpara.addEventListener('click',changeText);
 // fpara.removeEventListener('click',changeText);
 // optimal way
+
+// prevent default methods
 let anchor = document.getElementById("link");
+anchor.addEventListener('click',function(event){
+    event.preventDefault();
+    anchor.textContent="Dont Click Me"
+})
+
+// let paras = document.querySelectorAll('p');
+// for (let i=0; i<paras.length; i++){
+//     let para = paras[i];
+//     para.addEventListener('click',alertP);
+// }
+function alertP(clik){
+    if(clik.target.nodeName ==='SPAN') {
+        alert("You have clicked on para: "+ clik.target.textContent)}
+}
+let mydiv = document.getElementById('wrapper')
+document.addEventListener('click',alertP);
+
+document.addEventListener("DOMContentLoaded", ready);
+function ready() {
+    alert('DOM is ready');
+    alert(`Image size: ${img.offsetWidth}x${img.offsetHeight}`);
+  }
