3eb05b8c50a36b1240c5c900189ce59040b468a8
KHAGENDRA <mohanta.khagendranath@gmail.com>
2025-02-08 22:00:49 +0530

    fetch API

--- codehelp/JS/Promise/async-await.js
+++ codehelp/JS/Promise/async-await.js
@@ -4,3 +4,17 @@ async function getData() {
     }, 3000);
 }
 getData();
+
+//await
+
+//Fetch API
+async function data() {
+    //get request - async
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts/5');
+    // json parsing
+    let data = await response.json();
+    console.log(data);
+    // console.log(response); 
+}
+
+data();
